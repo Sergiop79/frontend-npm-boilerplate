e3887956c9084eb4db32de9e5d3e4fb7d3e32d78
Sergio Peña <sergiop79@gmail.com>
2018-09-28 12:32:36 +0200

    Added navigation toggle button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,33 @@
-const showMessage = () => console.log('Babel works');
+(function (c) {
 
-showMessage();
+  const showMessage = () => c('Babel works');
+  
+  showMessage()
+
+  // navigation
+  const navigationButton = document.getElementById('nav-toggle')
+  const navigation = document.getElementById('navigation')
+
+  const toggleNavigationButton = (e) => {
+    e.preventDefault();
+    if (navigationButton.classList.contains('is-open')) {
+      navigationButton.classList.remove('is-open')
+    } else {
+      navigationButton.classList.add('is-open')
+    }
+  }
+
+  const toggleNavigation = (e) => {
+    if (navigation.classList.contains('is-open')) {
+      navigation.classList.remove('is-open')
+    } else {
+      navigation.classList.add('is-open')
+    }
+  }
+
+  navigationButton.addEventListener('click', toggleNavigationButton)
+  navigationButton.addEventListener('click', toggleNavigation)
+
+
+}(console.log));
+
