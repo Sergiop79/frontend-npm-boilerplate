749326fa60bbbdbe82097e4d24165c8f0fe5a405
Sergio Peña <sergiop79@gmail.com>
2018-11-24 09:28:51 +0100

    Added svg icons and change navigation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,33 +1,15 @@
-(function (c) {
+(function () {
 
-  const showMessage = () => c('Babel works');
-  
-  showMessage()
+  const navigationButton = document.getElementById('nav-toggle');
+  const navigationClose = document.getElementById('navigation-close');
+  const navigation = document.getElementById('navigation');
 
-  // navigation
-  const navigationButton = document.getElementById('nav-toggle')
-  const navigation = document.getElementById('navigation')
+  navigationButton.addEventListener('click', () => {
+    navigation.classList.add('is-open');
+  });
 
-  const toggleNavigationButton = (e) => {
-    e.preventDefault();
-    if (navigationButton.classList.contains('is-open')) {
-      navigationButton.classList.remove('is-open')
-    } else {
-      navigationButton.classList.add('is-open')
-    }
-  }
+  navigationClose.addEventListener('click', () => {
+    navigation.classList.remove('is-open');
+  });
 
-  const toggleNavigation = (e) => {
-    if (navigation.classList.contains('is-open')) {
-      navigation.classList.remove('is-open')
-    } else {
-      navigation.classList.add('is-open')
-    }
-  }
-
-  navigationButton.addEventListener('click', toggleNavigationButton)
-  navigationButton.addEventListener('click', toggleNavigation)
-
-
-}(console.log));
-
+}());
